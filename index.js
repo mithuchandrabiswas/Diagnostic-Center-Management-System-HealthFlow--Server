267808d1cc20773ab8c14a00a1f2ca7b4b33fe7b
Mithu1424882 <biswasmithumcb@gmail.com>
2024-06-01 14:41:12 +0600

    Set mongoDB in this project

--- index.js
+++ index.js
@@ -1,23 +1,73 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 5000
+require('dotenv').config()
+const cors = require('cors')
+// const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId, Timestamp } = require('mongodb')
+// const jwt = require('jsonwebtoken')
+const port = process.env.PORT || 9000
 
-// config
-require("dotenv").config();
+// This is your test secret API key.
+// const stripe = require("stripe")(process.env.VITE_STRIPE_Secret_key);
 
 // middleware
-app.use(cors());
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
 app.use(express.json())
+// app.use(cookieParser())
 
 
 
-// 
-app.get('/', (req, res) => {
-    res.send('Server running')
+// Verify Token Middleware
+// const verifyToken = async (req, res, next) => {
+//   const token = req.cookies?.token
+//   // console.log(token)
+//   if (!token) {
+//     return res.status(401).send({ message: 'unauthorized access' })
+//   }
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       // console.log(err)
+//       return res.status(401).send({ message: 'unauthorized access' })
+//     }
+//     req.user = decoded
+//     // console.log('Decoded', decoded);
+//     next()
+//   })
+// }
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2bu9h7l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 })
 
-// Listen
-app.listen(port,(req,res) => {
-    console.log(`Server running is port number: ${port}`);
+async function run() {
+  try {
+
+   
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('Assigment 12 server is running')
+})
+
+app.listen(port, () => {
+  console.log(`Assigment 12 is running on port ${port}`)
 })
